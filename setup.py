1f69bf08825b1d5928f5212394a1ed86ba821d0a
Hobthrust <46998441+Hobthrust@users.noreply.github.com>
2019-01-24 13:28:21 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='ARBInterp',
+    version='1.0',
+    packages=['ARBTools',],
+    author=['Paul Walker'],
+    author_email='paul.a.walker@durham.ac.uk',
+    url='https://www.jqc.org.uk/',
+    description='Python tools for interpolating 3D fields',
+    long_description=open('README.txt', 'r').read(),
+    license='GPL'
+)
+
