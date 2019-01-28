6c1dfbd262dc6cd6387835e7e7678ee2a44a151c
Paul A. Walker <46998441+Hobthrust@users.noreply.github.com>
2019-01-28 14:53:21 +0000

    Update ARBExample.py

--- ARBExample.py
+++ ARBExample.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import numpy as np
-from ARBInterp import tricubic
+from ARBTools.ARBInterp import tricubic
 
 ######################################################################
 	
