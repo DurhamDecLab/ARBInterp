bd4c7daafb36997ae5eed85385d9f23abac762a0
Paul A. Walker <46998441+DurhamDecLab@users.noreply.github.com>
2019-02-15 12:33:50 +0000

    Add files via upload

--- ARBInterpExample.py
+++ ARBInterpExample.py
@@ -10,18 +10,22 @@ if __name__ == '__main__':
 	print ("--- Loading field ---")
 	field = np.genfromtxt(fieldname+'.csv', delimiter=',')
 	
-	Run = tricubic(field)
+	Run = tricubic(field)	# mode kword arg will be ignored if a scalar (Nx4) input is detected
 
 	coords=np.zeros((20,3))
 	coords[:,0]=np.linspace(-2e-3,2e-3,20)
 	coords[:,1]=np.linspace(-2e-3,2e-3,20)
 	coords[:,2]=np.linspace(-2e-3,2e-3,20)
 	
-	output = Run.sQuery((coords[3]))
+	output = Run.Query((coords[3]))
+	print '\n'
+	print 'Single point query, scalar field:'
 	print output
 	print '\n'
 	
-	Comps = Run.rQuery(coords)
+	Comps = Run.Query(coords)
+	print '\n'
+	print 'Multi point query, scalar field:'
 	print (Comps)
 
 	
@@ -30,11 +34,15 @@ if __name__ == '__main__':
 	print ("--- Loading field ---")
 	field = np.genfromtxt(fieldname+'.csv', delimiter=',')
 	
-	Run = tricubic(field, mode='both')
+	Run = tricubic(field, mode='both')	# mode options are 'both', 'vector' or 'norm', defaults to 'vector'. Pass arg 'quiet' to suppress setup text
 
-	output = Run.sQuery((coords[3]))
+	output = Run.Query((coords[3]))
+	print 'Single point query, vector field:'
+	print '\n'
 	print output
 	print '\n'
 	
-	Comps = Run.rQuery(coords)
+	Comps = Run.Query(coords)
+	print 'Multi point query, vector field:'
+	print '\n'
 	print (Comps)
--- ARBTrajecExample.py
+++ ARBTrajecExample.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
 	N = 100		# number of atoms
 	T = 0.01		# temperature in K
 
-	moment = 3	#	magnetic moment in Bohr magnetons - mJ x g-factor
-	# testing with Ar in 3P2 state = mJ = 2, low-field-seeking
+	moment = 3	#	magnetic moment in Bohr magnetons - mJ x g-factor (neglecting nuclear spin, use mF if I!=0)
+	# testing with Ar in 3P2 state: mJ = 2, low-field-seeking
 	
 	tmax = 0.01	# end time of simulation
-	timestep = 1e-6 # really should be 1e-7 for very accurate work but larger steps OK for testing
+	timestep = 1e-6 # acceptable timestep depends on shape of potential!
 
 	fieldname = "ExampleVectorField"
 	field = np.genfromtxt(fieldname+'.csv', delimiter=',') # field to be interpolated
