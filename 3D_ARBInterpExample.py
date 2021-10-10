854ebe7b2986c4cfe169da25d0474125ec8ce303
Paul A. Walker <46998441+DurhamDecLab@users.noreply.github.com>
2021-10-10 01:11:47 +0100

    Add files via upload

---
+++ 3D_ARBInterpExample.py
@@ -0,0 +1,50 @@
+import numpy as np
+from ARBTools.ARBInterp import tricubic
+from sys import platform
+
+######################################################################
+	
+if __name__ == '__main__':
+	if platform == "win32":
+		slash = "\\"
+	elif platform == "linux" or "linux2":
+		slash = "/"
+
+	print ("--- Loading field ---")
+	field=np.genfromtxt("ExampleFields"+slash+"Example3DScalarField.csv", delimiter=',')
+	
+	Run = tricubic(field)	# mode kword arg will be ignored if a scalar (Nx4) input is detected
+
+	coords=np.zeros((20,3))
+	coords[:,0]=np.linspace(-2e-3,2e-3,20)
+	coords[:,1]=np.linspace(-2e-3,2e-3,20)
+	coords[:,2]=np.linspace(-2e-3,2e-3,20)
+	
+	output = Run.Query((coords[3]))
+	print ('\n')
+	print ('Single point query, scalar field:')
+	print (output)
+	print ('\n')
+	
+	Comps = Run.Query(coords)
+	print ('\n')
+	print ('Multi point query, scalar field:')
+	print (Comps)
+	
+######################################################################
+
+	print ("--- Loading field ---")
+	field=np.genfromtxt("ExampleFields"+slash+"Example3DVectorField.csv", delimiter=',')
+	
+	Run = tricubic(field, mode='both')	# mode options are 'both', 'vector' or 'norm', defaults to 'vector'. Pass arg 'quiet' to suppress setup text
+
+	output = Run.Query((coords[3]))
+	print ('Single point query, vector field:')
+	print ('\n')
+	print (output)
+	print ('\n')
+	
+	Comps = Run.Query(coords)
+	print ('Multi point query, vector field:')
+	print ('\n')
+	print (Comps)
